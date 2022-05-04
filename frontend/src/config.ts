266b36ea003bc5ef69f18c9ee1d035184336f526
tonwayer <tonwayer@gmail.com>
2022-05-04 19:12:24 -0400

    Fixed UI

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,5 +3,5 @@ export const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET_SERVER
   : "ws://127.0.0.1:8001"
 
 export const FILE_SERVER_URL = process.env.REACT_APP_FILE_SERVER
-  ? process.env.REACT_APP_FILE_SERVER
-  : "http://127.0.0.1:8000"
+  ? process.env.REACT_APP_FILE_SERVER + "/recordings/"
+  : "http://127.0.0.1:8000/recordings/"
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,14 +1,15 @@
 import { useContext, useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 import { WebSocketContext } from "../WebSocketProvider";
 import { GET_RECORDINGS } from "../WebSocketProvider/messages";
 import WaveForm from "../components/WaveForm";
 import RecordingList from "../components/RecordingList";
-import Grid from "@mui/material/Grid";
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
+import { FILE_SERVER_URL } from "../config";
 
 export const Home = () => {
   const ws = useContext(WebSocketContext);
@@ -33,6 +34,7 @@ export const Home = () => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
+        <h2>Record voice</h2>
         <WaveForm value={ws?.peek ? ws.peek : 0} />
       </Grid>
       <Grid item xs={12} justifyContent="center">
@@ -44,15 +46,20 @@ export const Home = () => {
         </Button>
       </Grid>
       <Divider />
-      <Grid item xs={12} justifyContent="center">
-      {selectedFile && <AudioPlayer src={"http://127.0.0.1:8000/" + selectedFile} />}
-      {ws?.recordings && (
-        <RecordingList
-          handleSelectFile={handleSelectFile}
-          recordings={ws.recordings}
-          selectedFile={selectedFile}
-        />
-      )}
+      <Grid item md={5} justifyContent="center">
+        <h2>Recorded lists</h2>
+        {ws?.recordings
+          ? <RecordingList
+            handleSelectFile={handleSelectFile}
+            recordings={ws.recordings}
+            selectedFile={selectedFile}
+          />
+          : <p>No records</p>
+        }
+      </Grid>
+      <Grid item md={7} justifyContent="center">
+        <img src="/logo.svg" alt="logo" />
+        {selectedFile && <AudioPlayer src={FILE_SERVER_URL + selectedFile} />}
       </Grid>
     </Grid>
   );
